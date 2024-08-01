7e74841995621f2e507c38fc18454441eefaa8d1
T-800-888 <rayssagrafetti7@gmail.com>
2024-08-01 10:37:02 -0300

    08-01_ImprimirValores.py

---
+++ 08-01_ImprimirValores.py
@@ -0,0 +1,7 @@
+#Crie um algoritmo que recebe quatro informações e armazena em variáveis: Nome, CPF, Telefone e Ano de Nascimento. Em seguida, imprima na tela esses valores formatados.
+nome = str(input("Digite o seu nome: "))
+cpf = str(input("Digite o seu CPF: "))
+tel = str(input("Digite seu número de telefone: "))
+nasc = str(input("Digite sua data de nascimento: "))
+
+print("Seus dados são: ","\n",nome,"\n",cpf,"\n",tel,"\n",nasc)
