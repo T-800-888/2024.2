26fc457fff28d405e18fc4d15479a47e18e5e428
Rayssa G. Grafetti <rayssagrafetti7@gmail.com>
2025-02-27 11:06:07 -0300

    Delete 08-01_CalculoIdade.py

--- 08-01_CalculoIdade.py
+++
@@ -1,7 +0,0 @@
-#Escreva um algoritmo em Python para calcular a idade de alguém, sabendo-se seu ano de nascimento.
-
-ano = int(input("Digite seu ano de nascimento: "))
-
-idade = 2024 - ano
-
-print("Sua idade é: ", idade)
