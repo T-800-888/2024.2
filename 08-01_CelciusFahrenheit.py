57bb264a61bbac3a729722099ef1d39c3061e676
Rayssa G. Grafetti <rayssagrafetti7@gmail.com>
2025-02-27 11:06:16 -0300

    Delete 08-01_CelciusFahrenheit.py

--- 08-01_CelciusFahrenheit.py
+++
@@ -1,7 +0,0 @@
-#Leia do teclado a temperatura em Celsius e imprima o equivalente em Fahrenheit.
-#Fórmula: (X °C × 9/5) + 32
-
-celcius = float(input("Digite a temperatura em Celcius para obter a conversão: "))
-fahr = (celcius * (9/5)) + 32
-
-print(celcius," Celcius = ",fahr," Faharenheit")
